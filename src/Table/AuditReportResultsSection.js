368b09f7ca4ff96197916a3b8c909843635f76ee
Alex Aragon <Alex.Aragon@kingcounty.gov>
2016-08-26 09:06:09 -0700

    Refactored to reportFollow up

--- src/Table/AuditReportResultsSection.js
+++ src/Table/AuditReportResultsSection.js
@@ -1,4 +1,3 @@
-var ReactDOM = require('react-dom');
 var React = require('react');
 var moment = require('moment');
 
@@ -6,36 +5,32 @@ module.exports = React.createClass({
   displayName: 'AuditReportResultsSection',
   render: function () {
     var reportItems = [];
-    //var reports = this.props.audit.reports;
     var reportPubDate = this.props.audit.Date_Published;
     var formatedReportPubDate = moment(reportPubDate, 'MM-DD-YYYY').format('MMMM D, YYYY');
+    var reports = [];
+    var reportsStyle = {padding: {paddingLeft:'20px'}, size:{fontSize:'16px'}};
     
-    var reportFollowUpDateVisible = 'show';
     var reportFollowUpDate;
+
+    reports.push(<p key="1" style={reportsStyle.padding}>{this.props.audit.ReportType}: {formatedReportPubDate}</p>);
     if(this.props.audit.FollowUpDate) {
-      reportFollowUpDateVisible = 'show';
       reportFollowUpDate = moment(this.props.audit.FollowUpDate, 'MM-DD-YYYY').format('MMMM D, YYYY');
-    } else {
-        reportFollowUpDateVisible = 'hidden';
+      reports.push(<p key="0" style={reportsStyle.padding}>Follow-up report: {reportFollowUpDate}</p>);
     }
     
     var dept = this.props.audit.Dept;
     var auditName = this.props.audit.Project;
     var pageUrl = this.props.audit.PageURL;
-    var reportType = this.props.audit.ReportType;
     
     return (
     <div className="audit-report-item" style={{paddingBottom: '20px'}}>
-      <p>
+      <p style={reportsStyle.size}>
         <a target="_blank" href={pageUrl}>
           {auditName}
         </a>
       </p>
-      <p>
-        {dept}
-      </p>
-      <p style={{paddingLeft: '20px'}}><strong>{reportType}:</strong> {formatedReportPubDate}</p>
-      <p className={reportFollowUpDateVisible} style={{paddingLeft: '20px'}}><strong>Follow-up report:</strong> {reportFollowUpDate}</p>
+      <p>{dept}</p>
+      {reports}
     </div>
     );
   }
